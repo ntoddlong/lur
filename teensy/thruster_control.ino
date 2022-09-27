78a7f2d535c6a0e090bf858f084c9184071504a8
Nathaniel Todd-Long <ntoddlong@gmail.com>
2022-09-27 04:14:44 +0000

    Updating thruster control stuff, serial is hard

--- teensy/thruster_control.ino
+++ teensy/thruster_control.ino
@@ -1,3 +1,4 @@
+#include <Servo.h>
 const float thruster_config[6][8] = {
   /*
    *      1  2  3  4  5  6  7  8
@@ -17,6 +18,19 @@ const float thruster_config[6][8] = {
   { -1.0,  1.0,  1.0, -1.0,  0.0,  0.0,  0.0,  0.0  }
 };
 
+byte led = 0;
+byte t1_pin = 1;
+byte l_pin = 2;
+Servo thruster1;
+Servo thruster2;
+Servo thruster3;
+Servo thruster4;
+Servo thruster5;
+Servo thruster6;
+Servo thruster7;
+Servo thruster8;
+Servo lights;
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -25,11 +39,18 @@ void setup() {
    * ** setup pins
    * ** what else?
    */
-  Serial.println("This may be sent before your PC is able to receive");
+//  Serial.println("This may be sent before your PC is able to receive");
   while (!Serial && millis() < 15000) {
     // wait up to 15 seconds for Arduino Serial Monitor
   }
-  Serial.println("This line will definitely appear in the serial monitor");
+//  Serial.println("This line will definitely appear in the serial monitor");
+// Serial.begin(9600);
+ pinMode(led, OUTPUT);
+//  lights.attach(l_pin);
+// thruster1.attach(t1_pin);
+// thruster1.writeMicroseconds(1500); // send "stop" signal to ESC.
+// delay(7000); // delay to allow the ESC to recognize the stopped signal
+  delay(2000);
 }
 
 void loop() {
@@ -37,7 +58,7 @@ void loop() {
 
   /* ** read serial messages from jetson
    *    should we handle configuration of thruster power
-   *    on jetson or on teensy? probably jetson
+   *    on jetson or on teensy? probably teensy
    *      message format:
    *          [enum::control_type, value
    *      or 
@@ -48,13 +69,39 @@ void loop() {
    * ** send to thruster (I think using Servo.h)
    * ** ack to serial?
    */
+//   Serial.flush();
    int count = 0;
-   char buf[11];
-   while (count < 11) {
-    if (Serial.available()) {  // receive all 11 bytes into "buf"
+   char buf[256];
+   while (count < 256) {
+    if (Serial.available() == 0) {  // receive all 16 bytes into "buf"
       buf[count++] = Serial.read();
     }
     else break;
   }
   Serial.printf("Read %d bytes: %s\n", count, buf);
+
+  Serial.println("hey");
+  Serial.println(Serial.baud());
+//  
+//  while (Serial.available() == 0);
+//  
+//  int val = Serial.parseInt(); 
+//  
+//  if(val < 1100 || val > 1900)
+//  {
+//    Serial.println("not valid");
+//  }
+//  else
+//  {
+//    thruster1.writeMicroseconds(val); // Send signal to ESC.
+//  }
+//
+//    thruster1.writeMicroseconds(1500);
+//    delay(1000);
+//  lights.writeMicroseconds(0);
+//analogWrite(led, 0);
+//delay(1000);
+//analogWrite(led, 500);
+delay(250);
+     
 }
