abb28a9be8f36d3265e16298d622b48d2708f8cd
Nathaniel Todd-Long <ntoddlong@gmail.com>
2022-10-22 14:49:33 +0000

    Updated arduino library

--- teensy/lur/lur.cpp
+++ teensy/lur/lur.cpp
@@ -1,31 +1,85 @@
 #include "lur.h"
 
-namespace lur {
-  typedef struct Motors {
-    Mode  mode;
-    Servo thrusters[8];
+/*
+ *
+ ************
+ *  Motors  *
+ ************
+ *
+ */
+Motors::Motors() : mode(Mode::disarmed) { }
 
-    Motors() : mode(Mode::disarmed) {
-      for (int i = 0; i < 8; ++i) {
-        thrusters[i].attach(thruster_pins[i]);
-      }
-      for (int i = 0; i < 8; ++i) {
-        thrusters[i].writeMicroseconds(1500);
-      }
-    }
-  };
+void Motors::initialize() {
+  for (int i = 0; i < NUM_THRUSTERS; ++i) {
+    thrusters[i].attach(thruster_pins[i]);
+  }
+  for (int i = 0; i < NUM_THRUSTERS; ++i) {
+    thrusters[i].writeMicroseconds(1500);
+  }
+}
 
-  typedef struct Sonar {
-    int            baud;
-    int            timeout;
-    Ping1D         ping1d;
-    SoftwareSerial ping_serial;
+bool Motors::set_mode(Mode m) {
+  switch (m) {
+    case Mode::disarmed:
+      disarm();
+      mode = m;
+      break;
+    case Mode::depth_hold:
+    case Mode::position_hold:
+    case Mode::manual:
+    case Mode::stabilize:
+      mode = m;
+      break;
+  }
+  return true;
+}
 
-    Sonar(int b, int to) : ping_serial({sonar_pins[0], sonar_pins[1]}), ping1d(ping_serial), baud(b), timeout(to) {
-      ping_serial.begin(baud);
-      while (!ping1d.initialize() && millis() < timeout * 1000) {
-        // wait up to timeout seconds for ping1d
-      }
-    }
-  };
+void Motors::disarm() {
+  for (int i = 0; i < NUM_THRUSTERS; ++i) {
+    thrusters[i].writeMicroseconds(1500);
+  }
+  mode = Mode::disarmed;
+}
+
+bool Motors::set_power(const int (&values)[NUM_THRUSTERS]) {
+  if (mode == Mode::disarmed) return false;
+  for (int i = 0; i < NUM_THRUSTERS; ++i) {
+    thrusters[i].writeMicroseconds(values[i]);
+  }
+  return true;
+}
+
+void Motors::add_to_power_vector(int (&values)[NUM_THRUSTERS], const float (&config)[NUM_THRUSTERS], int val) {
+  for (int i = 0; i < sizeof(values) / sizeof(int); ++i) {
+    values[i] += config[i] * val;
+  }
+}
+
+int Motors::normalize(int n, int min=0, int max=100) {
+  return (MAX_THRUST - MIN_THRUST) * ((n - min) / (max - min)) + MIN_THRUST;
+}
+
+bool Motors::spin(int x, int y, int z, int roll, int pitch, int yaw) {
+  int values[NUM_THRUSTERS] = { 0 };
+  add_to_power_vector(values, thruster_config[0], normalize(x));
+  add_to_power_vector(values, thruster_config[1], normalize(y));
+  add_to_power_vector(values, thruster_config[2], normalize(z));
+  add_to_power_vector(values, thruster_config[3], normalize(roll));
+  add_to_power_vector(values, thruster_config[4], normalize(pitch));
+  add_to_power_vector(values, thruster_config[5], normalize(yaw));
+  return set_power(values);
+}
+
+/*
+ *
+ ***********
+ *  SONAR  *
+ ***********
+ *
+ */
+Sonar::Sonar() : ping_serial({sonar_pins[0], sonar_pins[1]}), device(ping_serial), baud(sonar_baud), timeout(sonar_timeout) {
+  ping_serial.begin(baud);
+  while (!device.initialize() && millis() < timeout)  {
+    // wait up to timeout seconds for ping1d
+  }
 }
--- teensy/lur/lur.h
+++ teensy/lur/lur.h
@@ -3,44 +3,66 @@
 
 #pragma once
 
+#include "Arduino.h"
 #include <Servo.h>
 #include "SoftwareSerial.h"
 #include "ping1d.h"
-#include "ping360.h"
 
-namespace lur {
-  const int thruster_pins[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
-  const int sonar_pins[2]    = { 0, 1 };
+const int NUM_THRUSTERS = 8;
+const int MAX_THRUST = 1900;
+const int MIN_THRUST = 1900;
+const int thruster_pins[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+const int sonar_pins[2]    = { 0, 1 };
+const int sonar_baud       = 9600;
+// in milliseconds
+const int sonar_timeout    = 1500;
 
-  const float thruster_config[6][8] = {
-    /*
-     *      1  2  3  4  5  6  7  8
-     * x
-     * y
-     * z
-     * roll
-     * pitch
-     * yaw
-     * 
-     */
-    {  1.0,  1.0, -1.0, -1.0,  0.0,  0.0,  0.0,  0.0  },
-    {  1.0, -1.0,  1.0, -1.0,  0.0,  0.0,  0.0,  0.0  },
-    {  0.0,  0.0,  0.0,  0.0,  1.0,  1.0,  1.0,  1.0  },
-    {  0.0,  0.0,  0.0,  0.0, -1.0,  1.0, -1.0,  1.0  },
-    {  0.0,  0.0,  0.0,  0.0,  1.0,  1.0, -1.0, -1.0  },
-    { -1.0,  1.0,  1.0, -1.0,  0.0,  0.0,  0.0,  0.0  }
-  };
+const float thruster_config[6][8] = {
+  /*
+   *      1  2  3  4  5  6  7  8
+   * x
+   * y
+   * z
+   * roll
+   * pitch
+   * yaw
+   * 
+   */
+  {  1.0,  1.0, -1.0, -1.0,  0.0,  0.0,  0.0,  0.0  },
+  {  1.0, -1.0,  1.0, -1.0,  0.0,  0.0,  0.0,  0.0  },
+  {  0.0,  0.0,  0.0,  0.0,  1.0,  1.0,  1.0,  1.0  },
+  {  0.0,  0.0,  0.0,  0.0, -1.0,  1.0, -1.0,  1.0  },
+  {  0.0,  0.0,  0.0,  0.0,  1.0,  1.0, -1.0, -1.0  },
+  { -1.0,  1.0,  1.0, -1.0,  0.0,  0.0,  0.0,  0.0  }
+};
 
-  enum Mode {
-    disarmed,
-    stabilize,
-    manual,
-    depth_hold,
-    position_hold,
-  };
+enum Mode {
+  disarmed,
+  stabilize,
+  manual,
+  depth_hold,
+  position_hold,
+};
 
-  struct Motors;
-  struct Sonar;
-}
+struct Motors {
+  Mode  mode;
+  Servo thrusters[NUM_THRUSTERS];
+  Motors();
+  void initialize();
+  bool set_mode(Mode m);
+  void disarm();
+  bool set_power(const int (&values)[NUM_THRUSTERS]);
+  void add_to_power_vector(int (&values)[NUM_THRUSTERS], const float (&config)[NUM_THRUSTERS], int val);
+  int normalize(int n, int min, int max);
+  bool spin(int x, int y, int z, int roll, int pitch, int yaw);
+};
+
+struct Sonar {
+  int            baud;
+  int            timeout;
+  Ping1D         device;
+  SoftwareSerial ping_serial;
+  Sonar();
+};
 
 #endif
