ae79f7d96e3ff9583182d924d942f5642922cdb1
Nathaniel Todd-Long <ntoddlong@gmail.com>
2022-10-29 19:12:14 +0000

    Added serial communication stuff for teensy<->jetson

---
+++ serial_stuff/serial/serial.ino
@@ -0,0 +1,27 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned int bytes_read = 0;
+  char buf[256]{0};
+  while (Serial.available() && bytes_read < 256) {
+    buf[bytes_read] = Serial.read();
+    if (buf[bytes_read] == 0b00000001) Serial.println("0b00000001");
+    if (buf[bytes_read] == 0b00000010) Serial.println("0b00000010");
+    if (buf[bytes_read] == 0b00000100) Serial.println("0b00000100");
+    if (buf[bytes_read] == 0b00001000) Serial.println("0b00001000");
+    if (buf[bytes_read] == 0b00010000) Serial.println("0b00010000");
+    if (buf[bytes_read] == 0b00100000) Serial.println("0b00100000");
+    if (buf[bytes_read] == 0b01000000) Serial.println("0b01000000");
+    if (buf[bytes_read] == 0b10000000) Serial.println("0b10000000");
+    ++bytes_read;
+  }
+  buf[bytes_read] = '\0';
+  Serial.flush();
+  if (bytes_read) Serial.printf("I'm teensy, I heard %s\n", buf);
+  delay(1000);
+}
